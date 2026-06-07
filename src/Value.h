ddc81f01287f0fb1d4e3058ed086bc760e7cc32a
Krishna Jawale <krishnajawale7@gmail.com>
2026-06-07 15:21:12 -0400

    Add more operations and overloads

--- src/Value.h
+++ src/Value.h
@@ -16,7 +16,7 @@ class Value {
 
         void backward(const std::shared_ptr<Value>& root) {
             // set grad of root node to 1 (dL/dL = 1)
-            this->grad = 1.0f;
+            root->grad = 1.0f;
 
             // topological sort
             std::vector<std::shared_ptr<Value>> topo;
@@ -49,6 +49,7 @@ class Value {
 };
 
 // GLOBAL OPERATOR OVERLOADS
+// Addition
 std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
     auto res = std::make_shared<Value>(a->data + b->data, "(" + a->label + " + " + b->label + ")");
     res->prev = {a, b};
@@ -62,6 +63,16 @@ std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, const std::sha
     return res;
 }
 
+std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, float b) {
+    auto b_ptr = std::make_shared<Value>(b, std::to_string(b));
+    return a + b_ptr; // use shared_ptr<Value> + shared_ptr<Value> overload above
+}
+
+std::shared_ptr<Value> operator+(float a, const std::shared_ptr<Value>& b) {
+    return b + a; // use shared_ptr<Value> + float overload above
+}
+
+// Multiplication
 std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
     auto res = std::make_shared<Value>(a->data * b->data, "(" + a->label + " * " + b->label + ")");
     res->prev = {a, b};
@@ -75,6 +86,54 @@ std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, const std::sha
     return res;
 }
 
+std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, float b) {
+    auto b_ptr = std::make_shared<Value>(b, std::to_string(b));
+    return a * b_ptr; // use shared_ptr<Value> * shared_ptr<Value> overload above
+}
+
+std::shared_ptr<Value> operator*(float a, const std::shared_ptr<Value>& b) {
+    return b * a; // use shared_ptr<Value> * float overload above
+}
+
+// Power function
+std::shared_ptr<Value> pow(const std::shared_ptr<Value>& a, float exponent) {
+    float p = std::pow(a->data, exponent);
+
+    auto res = std::make_shared<Value>(p, "(" + a->label + " ^ " + std::to_string(exponent) + ")");
+    res->prev = {a};
+    res->op = "^";
+
+    res->backward_step = [res, a, exponent]() {
+        a->grad += res->grad * exponent * std::pow(a->data, exponent - 1); // dL/da = dL/dres * dres/da = res->grad * exponent * a^(exponent-1)
+    };
+
+    return res;
+}
+
+// Division
+std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a * pow(b, -1.0f); // use shared_ptr<Value> * shared_ptr<Value> overload and power function overload above
+}
+
+// Negation
+std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a) {
+    return a * -1.0f; // use shared_ptr<Value> * float overload above
+}
+
+// Subtraction
+std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a + (-b); // use shared_ptr<Value> + shared_ptr<Value> and negation overloads above
+}
+
+std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a, float b) {
+    return a + (-b); // use shared_ptr<Value> + shared_ptr<Value> and negation overloads above
+}
+
+std::shared_ptr<Value> operator-(float a, const std::shared_ptr<Value>& b) {
+    return a + (-b); // use shared_ptr<Value> + shared_ptr<Value> and negation overloads above
+}
+
+// TanH activation function
 std::shared_ptr<Value> tanh(const std::shared_ptr<Value>& a) {
     float t = std::tanh(a->data);
 
@@ -85,6 +144,21 @@ std::shared_ptr<Value> tanh(const std::shared_ptr<Value>& a) {
     res->backward_step = [res, a, t]() {
         // float t = std::tanh(a->data);
         a->grad += res->grad * (1 - t * t); // dL/da = dL/dres * dres/da = res->grad * (1 - tanh^2(a->data))
+    };
+
+    return res;
+}
+
+// Exp function (e^x)
+std::shared_ptr<Value> exp(const std::shared_ptr<Value>& a) {
+    float e = std::exp(a->data);
+
+    auto res = std::make_shared<Value>(e, "exp(" + a->label + ")");
+    res->prev = {a};
+    res->op = "exp";
+
+    res->backward_step = [res, a, e]() {
+        a->grad += res->grad * e; // dL/da = dL/dres * dres/da = res->grad * exp(a->data)
     };
 
     return res;
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ int main () {
     // basic neuron
 
     // inputs
-    auto x1 = std::make_shared<Value>(1.0f, "x1");
-    auto x2 = std::make_shared<Value>(1.0f, "x2");
+    auto x1 = std::make_shared<Value>(2.0f, "x1");
+    auto x2 = std::make_shared<Value>(0.0f, "x2");
 
     // weights
-    auto w1 = std::make_shared<Value>(1.0f, "w1");
-    auto w2 = std::make_shared<Value>(-1.0f, "w2");
+    auto w1 = std::make_shared<Value>(-3.0f, "w1");
+    auto w2 = std::make_shared<Value>(1.0f, "w2");
 
     // bias
-    auto b = std::make_shared<Value>(0.0f, "b");
+    auto b = std::make_shared<Value>(6.8813735870195432f, "b");
 
     auto x1w1 = w1 * x1;
     x1w1->label = "x1w1";
@@ -29,8 +29,11 @@ int main () {
     auto d = x1w1x2w2 + b;
     d->label = "d";
 
+    auto e = exp(2.0f * d);
+    e->label = "e";
+
     // apply tanh
-    auto o = tanh(d);
+    auto o = (e - 1) / (e + 1);
     o->label = "o";
 
     std::cout << "o: " << o->data << std::endl;
